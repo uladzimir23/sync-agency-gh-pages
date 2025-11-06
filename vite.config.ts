47fef8218348cc9eb1b4350d90e16cf58b0faf03
uladzimir23 <49497624+uladzimir23@users.noreply.github.com>
2025-11-07 01:51:30 +0300

    Build preparation

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,7 @@ import { fileURLToPath } from 'url'
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
-  plugins: [
-    react(),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
@@ -21,8 +19,13 @@ export default defineConfig({
   build: {
     cssCodeSplit: true,
     sourcemap: false,
-    minify: 'esbuild'
+    minify: 'esbuild',
+    // Для GitHub Pages, если приложение размещается в подпапке
+    outDir: 'dist',
+    assetsDir: 'assets'
   },
+  // Если приложение размещается не в корне (например, username.github.io/repo-name)
+  base: './', // или process.env.NODE_ENV === 'production' ? '/repo-name/' : './'
   css: {
     postcss: './postcss.config.js'
   }
